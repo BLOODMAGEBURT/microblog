332e227159f5098c194a0550990d0bed42a9b6b9
burt <henghenghalala@163.com>
2019-01-30 00:07:51 +0800

    update reset password

--- app/routes.py
+++ app/routes.py
@@ -190,6 +190,10 @@ def reset_password(token):
     user = User.verify__reset_password_token(token)
     if not user:
         return redirect(url_for('index'))
-
     form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
     return render_template('reset_password.html', title='Reset Password', form=form)
